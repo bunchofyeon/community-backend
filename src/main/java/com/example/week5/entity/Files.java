e3676bbbb3cc9d5875d18df9cf31cd70e71246e6
cloud-yeonsong <dldusthd7@naver.com>
2025-11-01 17:03:09 +0900

    JWT first

--- src/main/java/com/example/week5/entity/Files.java
+++ src/main/java/com/example/week5/entity/Files.java
@@ -6,7 +6,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
